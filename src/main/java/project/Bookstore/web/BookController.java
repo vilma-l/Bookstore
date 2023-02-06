128e96c06985167f28b83909fef1ac1ed969dc59
Vilma Laurila <vilma.laurila@myy.haaga-helia.fi>
2023-02-06 16:13:32 +0200

    Adding listing page and new GET method

--- src/main/java/project/Bookstore/web/BookController.java
+++ src/main/java/project/Bookstore/web/BookController.java
@@ -1,11 +1,20 @@
 package project.Bookstore.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import project.Bookstore.domain.Book;
+import project.Bookstore.domain.BookRepository;
+
 @Controller
 public class BookController {
+	@Autowired
+	BookRepository bookRepository;
 	
 	//lähettää selaimeen etusivunäkymän html-dokumentin
 	
@@ -13,6 +22,17 @@ public class BookController {
 	public String getIndex() {
 		
 		return "bookstore"; //bookstore.html
+	}
+	
+	//kirjalistaus
+	
+	@RequestMapping(value = "/booklist", method = RequestMethod.GET)
+	public String getBooks(Model model) {
+		
+		List<Book> books = (List<Book>) bookRepository.findAll(); // haetaan tietokannasta
+		model.addAttribute("books", books); //välitetään lista kirjoista templatelle
+		
+		return "booklist"; //booklist.html
 	}
 
 }
