f1c666012e745f74345aa4cfc32f1364091c54f7
Vilma Laurila <vilma.laurila@myy.haaga-helia.fi>
2023-02-06 17:27:49 +0200

    Adding methods for add and delete, and the required thymeleaf templates

--- src/main/java/project/Bookstore/web/BookController.java
+++ src/main/java/project/Bookstore/web/BookController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -33,6 +35,33 @@ public class BookController {
 		model.addAttribute("books", books); //välitetään lista kirjoista templatelle
 		
 		return "booklist"; //booklist.html
+	}
+	
+	// tyhjä kirjalomake
+	
+	@RequestMapping(value = "/newbook", method = RequestMethod.GET)
+	public String getAddBookForm(Model model) {
+		model.addAttribute("book", new Book());
+		
+		return "addbook"; //addbook.html
+	}
+	
+	// lomakkeella syötettyjen tietojen vastaanotto ja tallennus
+	
+	@RequestMapping(value = "/savebook", method = RequestMethod.POST)
+	public String saveBook(@ModelAttribute Book book, Model model) {
+		bookRepository.save(book); // tallennetaan uusi kirja tietokantaan, jos id-arvo on 0 tai null
+		
+		return "redirect:/booklist";
+	}
+	
+	// kirjan poisto
+	
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String deleteBook(@PathVariable("id") Long id, Model model) {
+		bookRepository.deleteById(id);
+		
+		return "redirect:/booklist";
 	}
 
 }
